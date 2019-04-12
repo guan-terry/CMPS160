acfd9725f1b9c526afee396b0aa29addf732e42b
Terry <terryguan1@gmail.com>
2019-04-11 22:10:01 -0700

    finished

---
+++ Base code/lib/src/graphics/geometry/shapes/square.js
@@ -0,0 +1,49 @@
+/**
+ * Specifies a square. A subclass of geometry.
+ *
+ * @author Terry Guan
+ * @this {Square}
+ */
+class Square extends Geometry {
+  /**
+   * Constructor for Square.
+   *
+   * @constructor
+   * @param {Shader} shader Shading object used to shade geometry
+   * @returns {Square} Square created
+   */
+  constructor(shader,xPos, yPos, redVal, greenVal, blueVal, sizeMultiplier) {
+      super(shader);
+      console.log(xPos, yPos);
+      var xVal = (xPos-200)/200;  
+      var yVal = ((yPos-200)/200) * -1;
+
+      this.vertices = this.generateSquareVertices(xVal,yVal, redVal, greenVal, blueVal, sizeMultiplier);
+      this.faces = {0: this.vertices};
+
+      // CALL THIS AT THE END OF ANY SHAPE CONSTRUCTOR
+      this.interleaveVertices();
+  }
+
+  generateSquareVertices(xPos, yPos, redVal, greenVal, blueVal, shapeMultiplier) {
+      var vertices = []
+      //console.log("xPos is: " + xPos);
+      //console.log("yPos is: " + yPos);
+      var vertex1 = new Vertex( xPos-(0.135*shapeMultiplier), yPos+(0.135*shapeMultiplier), 0.0, redVal, greenVal, blueVal);
+      var vertex2 = new Vertex( xPos-(0.135*shapeMultiplier), yPos-(0.135*shapeMultiplier), 0.0, redVal, greenVal, blueVal);
+      var vertex3 = new Vertex( xPos+(0.135*shapeMultiplier), yPos+(0.135*shapeMultiplier), 0.0, redVal, greenVal, blueVal);
+      var vertex4 = new Vertex( xPos+(0.135*shapeMultiplier), yPos-(0.135*shapeMultiplier), 0.0, redVal, greenVal, blueVal);
+
+      /*console.log(xPos-0.135, yPos+0.135);
+      console.log(xPos-0.135, yPos-0.135);
+      console.log(xPos+0.135, yPos+0.135);
+      console.log(xPos+0.135, yPos-0.135);
+      */
+      vertices.push(vertex1);
+      vertices.push(vertex2);
+      vertices.push(vertex4);
+      vertices.push(vertex3);
+
+      return vertices;
+  }
+}
---
+++ Base code/lib/src/main.js
@@ -0,0 +1,46 @@
+
+var shader = null;
+
+function main() {
+  // Retrieve the canvas from the HTML document
+  canvas = document.getElementById("webgl");
+
+  // Retrieve WebGL rendering context
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log("Failed to get WebGL rendering context.");
+    return;
+  }
+  var shape = 1;
+  // Initialize the scene
+  var scene = new Scene();
+  var inputHandler = new InputHandler(canvas, scene, shape);
+
+  var clearCanvas = document.getElementById("clearCanvas");
+  clearCanvas.onclick = function() {
+    scene.clearGeometries();
+  }
+
+  var triangleButton = document.getElementById("triangleButton");
+  triangleButton.onclick = function() {
+    inputHandler.shapeObject = 1;
+  }
+  var squareButton = document.getElementById("squareButton");
+  squareButton.onclick = function() {
+    inputHandler.shapeObject = 2;
+  }
+  var circleButton = document.getElementById("circleButton");
+  circleButton.onclick = function() {
+    inputHandler.shapeObject = 3;
+  }
+  // Initialize shader
+  shader = new Shader(gl, ASG1_VSHADER, ASG1_FSHADER);
+
+  // Add attibutes
+  shader.addAttribute("a_Position");
+  shader.addAttribute("a_Color");
+
+  // Initialize renderer with scene and camera
+  renderer = new Renderer(gl, scene, null);
+  renderer.start();
+}
