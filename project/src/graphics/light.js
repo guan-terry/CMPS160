dfb73a9d770cfad510574fddb982f29e937f45ed
Terry <terryguan1@gmail.com>
2019-06-04 18:47:59 -0700

    made lighting more clear.

--- project/src/graphics/light.js
+++ project/src/graphics/light.js
@@ -1,8 +1,8 @@
 class Light {
     constructor(x, y, z) {
         this.pos = new Vector3([x, y, z]);
-        this.ambient = [0.5, 0.5, 0.5];
-        this.diffuse = [0.5, 0.5, 0.5];
-        this.specular = [0.5, 0.5, 0.5];
+        this.ambient = [0.1, 0.1, 0.1];
+        this.diffuse = [0.3, 0.3, 0.3];
+        this.specular = [0.9, 0.9, 0.9];
     }
 }
--- project/src/main.js
+++ project/src/main.js
@@ -15,7 +15,7 @@ function main() {
     return;
   }
   var camera = new Camera();
-  var light = new Light(-0.5, -0.5, 0.5);
+  var light = new Light(-1.3, -0.3, 0.5);
 
 
 
